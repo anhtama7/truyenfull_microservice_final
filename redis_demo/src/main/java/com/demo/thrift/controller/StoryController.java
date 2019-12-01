dd23ed39974f228ebb22222904a223fb317942e3
Your Name <your_name@your_mail.com>
2019-12-01 17:51:28 +0700

    final commit

---
+++ redis_demo/src/main/java/com/demo/thrift/controller/StoryController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.demo.thrift.controller;
+
+import com.demo.thrift.model.Comic;
+import com.hcmut.truyenfull.lib.RedisDatasService;
+import com.demo.thrift.repo.RedisRepository;
+import java.util.List;
+import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author phong
+ */
+@RestController
+public class StoryController implements RedisDatasService.Iface{
+
+    @Autowired
+    private RedisRepository redisRepository;
+
+    
+
+    @Override
+    public String GetComic(String name) throws TException {
+        List<Comic> comics = (List<Comic>) redisRepository.findAllComic();
+        int id = 0;
+        for(Comic comic :comics){
+            if(comic.getTitle().equals(name)){
+               id = comic.getId();
+            }
+        }
+        if(id != 0){
+            return redisRepository.findComic(id).toString();
+        }
+        return "false";
+        
+    }
+
+}
